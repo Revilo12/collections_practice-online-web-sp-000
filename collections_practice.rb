44cef797c1297c5ff31321a381997bcce31a4717
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-04 16:21:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,14 @@ end
 def sort_array_char_count(array)
   array.sort {|a,b| a.length <=> b.length}
 end
+
+def swap_elements_from_to(array,index,destination_index)
+  placeholder = array[destination_index]
+  array[destination_index] = array[index]
+  array[index] = placeholder
+  array
+end
+
+def swap_elements(array)
+  swap_elements_from_to(array,1,2)
+end
